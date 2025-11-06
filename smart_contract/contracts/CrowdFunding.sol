7591478f8e3a712c39b530fae1fec0e3fa02baf9
Mengwa Ugo <mengwa44@yahoo.com>
2025-11-06 19:48:20 +0100

    added createCampaign function to smart contract

--- smart_contract/contracts/CrowdFunding.sol
+++ smart_contract/contracts/CrowdFunding.sol
@@ -21,5 +21,49 @@ contract CrowdFunding {
     //like a dictionary that links each campaign ID to its corresponding Campaign details
     //so each IDs points to a Campaign structure
     mapping(uint256 => Campaign) public campaigns;
+
+    //Counter to keep track of the number of campaigns created
+    uint256 public numberOfCampaigns = 0;
+
+
+    // Function to create a new crowdfunding campaign
+    function createCampaign(// anyone that wants to start a campaign can call this function and provide the necessary details
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline,
+        string memory _image
+    ) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];// Create a new campaign at the current numberOfCampaigns index
+
+        // Ensure the deadline is in the future
+        require(
+            _deadline > block.timestamp,
+            "The deadline should be a date in the future."
+        );
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;// Increment the campaign counter
+
+        return numberOfCampaigns - 1; // Return the ID of the newly created campaign
+    }
+
+
+    // Function to donate to a specific campaign
+    function donateToCampaign() {}
+
+    // Function to get the list of donators and their donations for a specific campaign
+    function getDonators() {}
+
+    // Function to get all campaigns
+    function getCampaigns() {}
 }
 +
