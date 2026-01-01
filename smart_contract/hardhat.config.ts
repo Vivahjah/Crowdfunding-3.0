6957965ae970e0c6bbf551de152943be7afbfeb4
Mengwa Ugo <mengwa44@yahoo.com>
2026-01-01 17:05:44 +0100

    restarted web3

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,15 +1,38 @@
-import { HardhatUserConfig } from "hardhat/config";
+// import { HardhatUserConfig } from "hardhat/config";
+
+// const config: HardhatUserConfig = {
+//   solidity: {
+//     version: '0.8.9',
+//     settings: {
+//       optimizer: {
+//         enabled: true,
+//         runs: 200,
+//       },
+//     },
+//   },
+// };
+
+// export default config;
+
+
+import type { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+if (!process.env.PRIVATE_KEY) {
+  throw new Error("Please set your PRIVATE_KEY in a .env file");
+}
 
 const config: HardhatUserConfig = {
-  solidity: {
-    version: '0.8.9',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
+  solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: "https://ethereum-sepolia-rpc.publicnode.com",
+          accounts: [process.env.PRIVATE_KEY]
+    }
+  } 
 };
 
 export default config;
