9e467cbad010f0421ad3526f6f298ea4a48c6f67
Mengwa Ugo <mengwa44@yahoo.com>
2026-01-03 01:55:18 +0100

    added constant and assests folders and files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 
-import thirdwebIcon from "./thirdweb.svg";
+
 
 
 export function App() {
 	return (
-		<main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
+		<main>
 			<h1>Hello Thirdweb</h1>
 		</main>
 	);
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import { ThirdwebProvider } from "thirdweb/react";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThirdwebProvider>
-      <App />
+    <ThirdwebProvider >
+    
+        <App />
+    
     </ThirdwebProvider>
   </React.StrictMode>
 );
